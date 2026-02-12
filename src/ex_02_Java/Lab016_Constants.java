6c58ee4419496701f62fadd403a40f3de193046e
Pankhuri <pankhurijain97@gmail.com>
2026-02-12 22:55:51 +0530

    newfile

--- src/ex_02_Java/Lab016_Constants.java
+++ src/ex_02_Java/Lab016_Constants.java
@@ -1,4 +1,12 @@
 package ex_02_Java;
 
 public class Lab016_Constants {
+    static void main(String[] args) {
+
+        final float PI = 3.14f;
+
+        final int num = 123;
+
+        //num = 123; PI = 3.122f // not possible
+    }
 }
