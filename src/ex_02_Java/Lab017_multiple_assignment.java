cd60e2b367730d2a054da424e07b635458e29e05
Pankhuri <pankhurijain97@gmail.com>
2026-02-12 23:00:00 +0530

    new file

---
+++ src/ex_02_Java/Lab017_multiple_assignment.java
@@ -0,0 +1,11 @@
+package ex_02_Java;
+
+public class Lab017_multiple_assignment {
+
+    static void main(String[] args) {
+
+        int a = 10 , b = 20, c = 30;
+        char n = 'n', f = 'f';
+
+    }
+}
