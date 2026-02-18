f80c8b44eebbaefe41f3c330433649b117fd7c7c
Pankhuri <pankhurijain97@gmail.com>
2026-02-18 22:42:45 +0530

    new file

--- src/ex_05_Type_Casting/Lab029_Explicit_Narrowing.java
+++ src/ex_05_Type_Casting/Lab029_Explicit_Narrowing.java
@@ -1,4 +1,12 @@
 package ex_05_Type_Casting;
 
 public class Lab029_Explicit_Narrowing {
+
+    static void main(String[] args) {
+        int a = 300;
+
+        byte num = (byte) a; // will print 44 after conversion to bits
+
+        System.out.println(num);
+    }
 }
