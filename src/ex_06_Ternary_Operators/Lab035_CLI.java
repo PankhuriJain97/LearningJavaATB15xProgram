2457cdcf74fc772107dc8362d9aa8264090abccf
Pankhuri <pankhurijain97@gmail.com>
2026-02-19 22:04:17 +0530

    new

---
+++ src/ex_06_Ternary_Operators/Lab035_CLI.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operators;
+
+public class Lab035_CLI {
+    static void main(String[] args) {
+
+        String age = args[0];
+        int my_age = Integer.parseInt(age); //wrapper method to covert string to int
+        System.out.println(age);
+    }
+}
