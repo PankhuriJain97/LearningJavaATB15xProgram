bd9279611d99ac52c07f99a79c081cffeae96a74
Pankhuri <pankhurijain97@gmail.com>
2026-02-19 23:01:53 +0530

    new

---
+++ src/ex_08_If_Statement/Lab038_If_else.java
@@ -0,0 +1,14 @@
+package ex_08_If_Statement;
+
+public class Lab038_If_else {
+    static void main(String[] args) {
+        int age = 15;
+
+        if(age>=18)
+        {
+            System.out.println("you can vote");
+        }
+        else
+            System.out.println("you cannot vote");
+    }
+}
