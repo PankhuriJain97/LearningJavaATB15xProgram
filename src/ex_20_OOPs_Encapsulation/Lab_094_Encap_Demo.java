d7bff394f139901994343ac3f5e8bb986cac5d6a
Pankhuri <pankhurijain97@gmail.com>
2026-03-12 12:27:17 +0530

    new

--- src/ex_20_OOPs_Encapsulation/Lab_094_Encap_Demo.java
+++ src/ex_20_OOPs_Encapsulation/Lab_094_Encap_Demo.java
@@ -10,7 +10,16 @@ public class Lab_094_Encap_Demo {
         l1.password = "newpass";
         System.out.println(l1.password);
 
+        Good_Login l2 = new Good_Login("pankh2", "pass2");
 
+        System.out.println(l2.getPassword());
+        System.out.println(l2.getUsername());
+
+        l2.setPassword("newpass2");
+        l2.setUsername("pankh3");
+
+        System.out.println(l2.getPassword());
+        System.out.println(l2.getUsername());
 
 
 
@@ -33,10 +42,33 @@ class Login{
 
 //encapsulated
 class Good_Login{
-
     private String username;
     private String password;
 
+    public Good_Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+ public void setUsername(String username)
+ {
+     this.username = username;
+ }
+
+ public void setPassword(String password)
+ {
+     this.password = password;
+ }
+
+ public String getUsername()
+ {
+     return username;
+ }
+
+ public  String getPassword()
+ {
+     return password;
+ }
 
 
 }
