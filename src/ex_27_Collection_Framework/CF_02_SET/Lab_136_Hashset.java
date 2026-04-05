d314f208b29333edbbe3ec79e788864dbb435470
Pankhuri <pankhurijain97@gmail.com>
2026-04-05 22:54:31 +0530

    new

---
+++ src/ex_27_Collection_Framework/CF_02_SET/Lab_136_Hashset.java
@@ -0,0 +1,20 @@
+package ex_27_Collection_Framework.CF_02_SET;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lab_136_Hashset {
+
+    static void main(String[] args) {
+
+        Set hs = new HashSet(); // we can give initial capacity
+
+        hs.add("Pankhuri");
+        hs.add("jain");
+        hs.add("jain");//this will be ignored since this is duplicate
+
+        hs.add("Jain"); //allowed since equals() does not ignore case
+
+        System.out.println(hs);
+    }
+}
