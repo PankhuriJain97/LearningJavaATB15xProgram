ad5c103a97ced662078f944707dfec9c2ac82935
Pankhuri <pankhurijain97@gmail.com>
2026-04-10 22:56:26 +0530

    new

---
+++ src/ex_29_Multithreading/Lab_149_TH01.java
@@ -0,0 +1,17 @@
+package ex_29_Multithreading;
+
+public class Lab_149_TH01 {
+    static void main(String[] args) throws InterruptedException{
+
+        Thread t = Thread.currentThread();
+
+        System.out.println(t);
+
+        for(int i =0; i <10; i++) {
+
+            System.out.println(i + " - " + t.getName());
+            System.out.println(i + " - " + t.getPriority());
+            Thread.sleep(5000);
+        }
+    }
+}
